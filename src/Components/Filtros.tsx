4c427e92c788daa474e09c32776f3ec9b7b47998
Juan Tardivo <juanaugustptardivo@hotmail.com>
2023-07-11 12:24:24 -0300

    filtro arrancado

--- src/Components/Filtros.tsx
+++ src/Components/Filtros.tsx
@@ -1,78 +1,61 @@
-import React from "react";
-import {useState} from "react";
-import {
-    TextField, 
-    IconButton, 
-    FormLabel, 
-    Checkbox, 
-    FormGroup, 
-    FormControlLabel, 
-    FormControl,
-    RadioGroup,
-    Radio
-} from "@mui/material";
-import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
-import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
-import TravelExploreIcon from '@mui/icons-material/TravelExplore';
-import './Styled/FiltrosStyle.css';
+// import * as React from 'react';
+// import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+// import dayjs, { Dayjs } from 'dayjs';
+// import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+// import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+// import { DateField } from '@mui/x-date-pickers/DateField';
+// import "./Styled/FiltrosStyle.css"
 
-interface BuscadorProps{
-    setSearchQuery: React.Dispatch<React.SetStateAction<string>>,
-}
+// export default function CustomDateFormat() {
+//     const [value, setValue] = React.useState<Dayjs | null>(dayjs(new Date()));
 
+//     return (
+//       <div className='filtros'>
+//       <LocalizationProvider dateAdapter={AdapterDayjs}>
+//         <DemoContainer components={['DateField']}>
+//           <DateField
+//             color="primary"
+//             label="Select Date"
+//             value={value}
+//             disableFuture={true}
+//             onChange={(newValue) => setValue(newValue)}
+//             format="DD-MM-YYYY"
+//           />
+//         </DemoContainer>
+//       </LocalizationProvider>
+//       </div>
+//     );
+//   };
 
-const Filtros = () => {
-    // const [search,setSearch]=useState("");
-    // function handleSearchChange(e:React.ChangeEvent<HTMLInputElement>){
-    //     setSearch(e.target.value);
-    // }
-    // function handleSearch(){
-    //     setSearchQuery(search);
-    // }
-    return(
-            // <div className="filtros">
+import * as React from "react";
+import dayjs, { Dayjs } from "dayjs";
+import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import "./Styled/FiltrosStyle.css";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import { IconButton } from "@mui/material";
 
+export default function DateCalendarValue() {
+  const [value, setValue] = React.useState<Dayjs | null>(dayjs(new Date()));
 
-
-
-
-
-
-            // </div>
-
-
-       <div className="filtros">
-        <FormControl className="fulltime" component="fieldset">
-            <FormControlLabel
-                value="fulltime"
-                control={<Checkbox color="primary" />}
-                label="FullTime"
-                labelPlacement="end"
+  return (
+    <div className="filtros">
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DemoContainer components={["DateCalendar", "DateCalendar"]}>
+          <DemoItem>
+            <DateCalendar
+              value={value}
+              onChange={(newValue) => setValue(newValue)}
             />
-        </FormControl>
-        <h4 className="text">LOCATION</h4>
-        <div className="search">
-        <IconButton color="primary" type="submit">
-            <TravelExploreIcon/>
-        </IconButton>
-        <TextField className="form__input"
-            id="location"
-            name="location"
-            label="City, state, zip code or country"
-            variant="standard"
-            size="small"
-            // value={search}
-            // onChange={handleSearchChange}
-            required>
-        </TextField>
-        </div>
-       <FormControl className="countries" component="fieldset">
-        <FormControlLabel value="London" control={<Radio />} label="London" />
-        <FormControlLabel value="Amsterdam" control={<Radio />} label="Amsterdam" />
-        <FormControlLabel value="New York" control={<Radio />} label="New York" />
-        <FormControlLabel value="Berlin" control={<Radio />} label="Berlin" />
-        </FormControl>
-       </div>
-    );
-};
-export default Filtros;
+          </DemoItem>
+        </DemoContainer>
+      </LocalizationProvider>
+      <button className="filtrar">
+        {/* <CalendarMonthIcon color="primary"></CalendarMonthIcon> */}
+        <p className="text">Filtrar</p>
+      </button>
+    </div>
+  );
+}
